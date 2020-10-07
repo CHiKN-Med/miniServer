9ef2caefe7f13867b62aaccd3074297681265e54
Francis-KI <56248688+Francis-KI@users.noreply.github.com>
2020-10-07 16:15:37 +0200

    Update Main.java

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -2,6 +2,7 @@ package Client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,19 +15,26 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        scene2(primaryStage);
+        HBox textPane = new HBox(10);
+        HBox knapPane = new HBox(10);
+        HBox mainPane = new HBox(10);
+        TextField ta = new TextField();
+        TextField ta2 = new TextField();
+        Button knap = new Button("press here");
+        textPane.getChildren().addAll(ta, ta2);
+        knapPane.getChildren().addAll(knap);
+        mainPane.getChildren().addAll(textPane, knapPane);
+        textPane.setAlignment(Pos.TOP_LEFT);
+        knapPane.setAlignment(Pos.BOTTOM_RIGHT);
+        Scene scene = new Scene(mainPane, 300, 275);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("QUIZ");
         primaryStage.show();
     }
 
-    void scene2(Stage primaryStage){
-        HBox pane = new HBox(10);
-        TextField ta = new TextField();
-        Button knap = new Button("press here");
-        pane.getChildren().addAll(ta, knap);
-        Scene scene = new Scene(pane, 300, 275);
-        primaryStage.setScene(scene);
-    }
+
+
+
 
 
     public static void main(String[] args) {
