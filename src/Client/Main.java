f0072ec8564bc9578bf3b153cfe521bee37a206d
Emil <lort@gmail.com>
2020-10-07 15:41:26 +0200

    Uden XML

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -4,16 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        scene2(primaryStage);
         primaryStage.setTitle("QUIZ");
-        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+    }
+
+    void scene2(Stage primaryStage){
+        HBox pane = new HBox(10);
+        Scene scene = new Scene(pane, 300, 275);
+        primaryStage.setScene(scene);
     }
 
 
